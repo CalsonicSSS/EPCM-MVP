0c39c9d91a51e4a3d974a8a93cd92df3ac45d38a
CalsonicSKC1005 <calson@withautomatic.com>
2024-08-16 11:13:39 -0400

    update max timing again

--- src/app/api/identify-high-potential-bid/route.ts
+++ src/app/api/identify-high-potential-bid/route.ts
@@ -3,7 +3,7 @@
 import { openai, predefinedKeywords } from '@/utils/declarations';
 import { NextRequest, NextResponse } from 'next/server';
 
-export const maxDuration = 30;
+export const maxDuration = 60;
 export const dynamic = 'force-dynamic';
 
 export async function POST(req: NextRequest) {
